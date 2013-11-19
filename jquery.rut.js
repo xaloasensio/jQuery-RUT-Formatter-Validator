7b0c675a96b505f3b9a1946dd7b74f43151f63cc
xaloasensio <xaloasensio@users.noreply.github.com>
2013-11-19 16:53:38 -0300

    Update jquery.rut.js

--- jquery.rut.js
+++ jquery.rut.js
@@ -1,37 +1,65 @@
 (function($){
     $.fn.rut = function(options){
         var settings = $.extend({
-            validateOn: 'blur',
-            formatOn: 'keyup',
-            restrictKeys: true,
-            maxLength: 12,
-            permittedCharacters: '0123456789kK.-',
-            flag: function(){return true;}
+            largoRut: 12,
+            caracteresPermitidos: '0123456789kK',
+            condicionar: function(){return true;},
+            onError: function(){},
+            onSuccess: function(){}
         }, options);
         
-        function formatRut(){
-            if(!settings.flag()){
+        function limitarEntradaCaracteres(e){
+            if(!settings.condicionar()){
                 return;
             }
-            var entry = $(this).val();
-            if(settings.restrictKeys){
-                var restrictedValue = '';
-                for(i=0;i<entry.length;i++){
-                    if(settings.permittedCharacters.indexOf(entry.charAt(i)) !== -1){
-                        restrictedValue += entry.charAt(i);
-                    }
-                }
-                entry = restrictedValue;
-            }
-            $(this).val(entry.substr(0, settings.maxLength));
+            var chr = String.fromCharCode((e.which == null) ? e.keyCode : e.which);
+            return (settings.caracteresPermitidos.indexOf(chr) !== -1);
         }
         
-        function validateRut(){
-            console.log('validate');
+        function formateaRut(){
+            if(!settings.condicionar()){
+                return;
+            }
+            $this = sanitizar($(this).val());
+            var dv = $this.slice(-1);
+            var rut = $this.substr(0, $this.length - 1);
+            var rutFormateado = separadorDecimal(rut) + (rut.length > 0 ? "-" : "") + dv;
+            $(this).val(rutFormateado.slice(0, settings.largoRut));
+        }
+        
+        function validaRut(){
+            if(!settings.condicionar()){
+                return;
+            }
+            $this = sanitizar($(this).val());
+            var dv = $this.slice(-1).toLowerCase();
+            var rut = $this.substr(0, $this.length - 1);
+            var m = 0, s = 1;
+            for( ; rut; rut = Math.floor(rut/10)){
+                s = (s + rut % 10 * (9 - m++ % 6)) % 11;
+            }
+            
+            if (dv == (s ? s - 1 : 'k')) {
+                settings.onSuccess();
+            }else{
+                settings.onError();
+            }
+        }
+        
+        function sanitizar(s){
+            return s.replace(/\.|\-/g,"");
+        }
+        
+        function separadorDecimal(n){
+            var ret = "";
+            for(var i = n.length-1, j = 0; i >= 0; i--, j++){
+                ret = n.charAt(i) + (j > 0 && j % 3 == 0 ? "." : "") + ret;
+            }
+            return ret;
         }
         
         return this.each(function(){
-            $(this).on(settings.formatOn, formatRut).on(settings.validateOn, validateRut);
+            $(this).on('keypress',limitarEntradaCaracteres).on('keyup', formateaRut).on('blur', validaRut);
         });
     }
 })(jQuery);
